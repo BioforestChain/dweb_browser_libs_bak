e6756b47dfa59ecc75e0a7505199ec822b3ae4ca
Gaubee <GaubeeBangeel@Gmail.com>
2024-03-02 11:24:52 +0800

    :sparkles: 构建了reverse_proxy的desktop

---
+++ rust_library/reverse_proxy/src/desktopMain/kotlin/reverse_proxy/UniFFILib.kt
@@ -0,0 +1,56 @@
+package reverse_proxy
+
+
+
+import com.sun.jna.Library
+import com.sun.jna.Native
+
+@Synchronized
+private fun findLibraryName(): String {
+    val componentName = "reverse_proxy"
+    val libOverride = System.getProperty("uniffi.component.$componentName.libraryOverride")
+    if (libOverride != null) {
+        return libOverride
+    }
+    return "reverse_proxy"
+}
+
+actual object UniFFILib : Library {
+    init {
+        Native.register(UniFFILib::class.java, findLibraryName())
+        FfiConverterTypeVoidCallback.register(this)
+        
+    }
+
+    @JvmName("ffi_reverse_proxy_6e76_VoidCallback_init_callback")
+    actual external fun ffi_reverse_proxy_6e76_VoidCallback_init_callback(`callbackStub`: ForeignCallback,
+    _uniffi_out_err: RustCallStatus
+    ): Unit
+
+    @JvmName("reverse_proxy_6e76_start")
+    actual external fun reverse_proxy_6e76_start(`backendPort`: UShort,`onReady`: ULong,
+    _uniffi_out_err: RustCallStatus
+    ): Unit
+
+    @JvmName("ffi_reverse_proxy_6e76_rustbuffer_alloc")
+    actual external fun ffi_reverse_proxy_6e76_rustbuffer_alloc(`size`: Int,
+    _uniffi_out_err: RustCallStatus
+    ): RustBuffer
+
+    @JvmName("ffi_reverse_proxy_6e76_rustbuffer_from_bytes")
+    actual external fun ffi_reverse_proxy_6e76_rustbuffer_from_bytes(`bytes`: ForeignBytes,
+    _uniffi_out_err: RustCallStatus
+    ): RustBuffer
+
+    @JvmName("ffi_reverse_proxy_6e76_rustbuffer_free")
+    actual external fun ffi_reverse_proxy_6e76_rustbuffer_free(`buf`: RustBuffer,
+    _uniffi_out_err: RustCallStatus
+    ): Unit
+
+    @JvmName("ffi_reverse_proxy_6e76_rustbuffer_reserve")
+    actual external fun ffi_reverse_proxy_6e76_rustbuffer_reserve(`buf`: RustBuffer,`additional`: Int,
+    _uniffi_out_err: RustCallStatus
+    ): RustBuffer
+
+    
+}
