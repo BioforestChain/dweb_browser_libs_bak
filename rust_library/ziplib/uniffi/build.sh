4d05e59d5186f89342af7ae9e6acf7d61cccc67d
kingsword09 <kingsword09@gmail.com>
2024-06-05 14:47:29 +0800

    :sparkles: 添加 Windows 系统 Arm64 的支持

--- rust_library/ziplib/uniffi/build.sh
+++ rust_library/ziplib/uniffi/build.sh
@@ -54,9 +54,13 @@ cp -r ./target/x86_64-apple-darwin/release/libziplib.dylib ../src/desktopMain/re
 
 echo "cargo building x86_64-pc-windows-gnu..."
 cargo build --target x86_64-pc-windows-gnu --release --quiet
+echo "cargo building aarch64-pc-windows-msvc..."
+cargo build --target aarch64-pc-windows-msvc --release --quiet
 
 mkdir -p ../src/desktopMain/resources/win32-x86-64/
 cp -r ./target/x86_64-pc-windows-gnu/release/ziplib.dll ../src/desktopMain/resources/win32-x86-64/
+mkdir -p ../src/desktopMain/resources/win32-aarch64/
+cp -r ./target/aarch64-pc-windows-msvc/release/ziplib.dll ../src/desktopMain/resources/win32-aarch64/
 
 # jvm
 cp -r ./target/bindings/jvmMain/ ../src/desktopMain
