f189db9b56ed6622b4dca7dac44b8f987dce1b54
Kingsword <kingsword09@gmail.com>
2024-10-16 16:23:50 +0800

    :sparkles: publish to maven

---
+++ rust_library/buildSrc/src/main/kotlin/Publication.kt
@@ -0,0 +1,33 @@
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.assign
+
+
+fun MavenPublication.configurePom(project: Project) {
+  pom {
+    name = project.name
+    description = project.description
+    url =
+      "https://github.com/BioforestChain/dweb_browser_libs/tree/main/rust_library/${project.name}"
+    inceptionYear.set("2024")
+
+    licenses {
+      license {
+        name.set("MIT")
+        url.set("https://opensource.org/licenses/MIT")
+      }
+    }
+    // Specify developers information
+    developers {
+      developer {
+        id.set("BioforestChain")
+        name.set("dweb")
+        email.set("developer@bfchain.org")
+      }
+    }
+    // Specify SCM information
+    scm {
+      url.set("https://github.com/BioforestChain/dweb_browser_libs")
+    }
+  }
+}
--- rust_library/buildSrc/src/main/kotlin/publish-plugin.gradle.kts
+++ rust_library/buildSrc/src/main/kotlin/publish-plugin.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Locale
+
 plugins {
   `maven-publish` // 发布插件
   signing // 签名插件，发布的时候需要对包进行签名
@@ -5,57 +7,39 @@ plugins {
 
 val localProperties = project.localProperties()
 
+/// 将签名信息写入properties中
 ext["signing.keyId"] = localProperties.getString("signing.keyId")
 ext["signing.secretKeyRingFile"] =
   rootDir.resolve(localProperties.getString("signing.secretKeyRingFile")).absolutePath
 ext["signing.password"] = localProperties.getString("signing.password")
 
+/// 生成javadoc，否则desktop目标无法同步到Central
+val javadocJar = tasks.register("javadocJar", Jar::class.java) {
+  archiveClassifier.set("javadoc")
+}
+
 afterEvaluate {
   publishing {
     publications {
-      // 发布一个统一的 multiplatform 版本，整合所有支持的目标平台
-      create<MavenPublication>("multiplatform") {
-        from(components["kotlin"])  // 自动发布所有 Kotlin Multiplatform 支持的目标平台
-        groupId = "io.github.dweb-channel"
+      withType<MavenPublication> {
         group = "io.github.dweb-channel"
-        artifactId = project.name
+        groupId = "io.github.dweb-channel"
         version = project.version.toString()
-        pom {
-          name = project.name
-          description = project.description
-          url =
-            "https://github.com/BioforestChain/dweb_browser_libs/tree/main/rust_library/${project.name}"
-          inceptionYear.set("2024")
-
-          licenses {
-            license {
-              name.set("MIT")
-              url.set("https://opensource.org/licenses/MIT")
-            }
-          }
-          // Specify developers information
-          developers {
-            developer {
-              id.set("BioforestChain")
-              name.set("dweb")
-              email.set("developer@bfchain.org")
-            }
-          }
-          // Specify SCM information
-          scm {
-            url.set("https://github.com/BioforestChain/dweb_browser_libs")
-          }
+        if (name == "kotlinMultiplatform") {
+          artifactId = project.name
+        } else {
+          artifactId = project.name + "-${name.lowercase(Locale.getDefault())}"
         }
+        artifact(javadocJar)
+        configurePom(project)
       }
     }
+
     repositories {
       // 远程 Maven 仓库
       maven {
-        this.artifactUrls
-//        name = project.name
-        // change URLs to point to your repos, e.g. http://my.org/repo
-//        val releasesRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
-        val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        val releasesRepoUrl =
+          uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
         val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
         url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
         credentials {
@@ -71,10 +55,18 @@ afterEvaluate {
       gradle.taskGraph.allTasks.any { it is PublishToMavenRepository }
     }
 
-    sign(publishing.publications["multiplatform"])
+    /// 使用命令行签名的方式
+    useGpgCmd()
+    sign(publishing.publications)
   }
 
   tasks.withType<Sign> {
     onlyIf { !project.version.toString().endsWith("SNAPSHOT") }
+  }
+
+  /// 发布任务必须在签名任务之后
+  tasks.withType<AbstractPublishToMaven>().configureEach {
+    val signingTasks = tasks.withType<Sign>()
+    mustRunAfter(signingTasks)
   }
 }
